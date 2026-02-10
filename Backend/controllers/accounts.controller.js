10870512d3458feaf4559233d418915581f9b8e6
Sohel Sheikh <sheikhsohel1007@gmail.com>
2026-02-10 22:07:20 +0530

    signup done

--- Backend/controllers/accounts.controller.js
+++ Backend/controllers/accounts.controller.js
@@ -1,17 +1,34 @@
 import prisma from "../lib/prisma.js";
 import bcrypt from "bcrypt";
-export const signup = async(req, res) => {
-  const data=req.body;
-  if(data.password)
-    data.password=bcrypt.hashSync(data.password,10);
-  else
-    data.password="";
-  const user =await  prisma.user.create({
-    data: data,
-  })
-  res.status(201).json({data:user,message:"User created successfully"});
-};
+export const signup = async (req, res) => {
+  try {
+    const { password, ...rest } = req.body;
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email: req.body.email,
+      },
+    })
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const user = await prisma.user.create({
+      data: {
+        ...rest,
+        password: password
+          ? await bcrypt.hash(password, 10)
+          : null,
+        role: "user",
+      },
+    });
 
+    res.status(201).json({
+      message: "User created successfully",
+      data: user,
+    });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
 export const login = async(req, res) => {
   const data=req.body;
 //   console.log(data);
