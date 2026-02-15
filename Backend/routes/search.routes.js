5c1b012f7e24e1f730a3ec762119f8ce8d3d8e06
Sohel Sheikh <sheikhsohel1007@gmail.com>
2026-02-15 17:04:19 +0530

    clean work

--- Backend/routes/search.routes.js
+++ Backend/routes/search.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { verifyToken } from "../middlewares/verifytoken.js";
 import { searchServices, searchGroomers} from "../controllers/search.controller.js";
 const router = Router();
-
+//adding search
 router.get("/services", searchServices);
 router.get("/groomers", searchGroomers);
 export default router;
